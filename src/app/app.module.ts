d921c730f59ef0a9a2e9fd3e99d3036e7d934e27
Alex <alexongithub@gmail.com>
2018-10-25 09:34:09 -0400

    add modules to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { NgOidcClientModule } from 'ng-oidc-client';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot([]),
+    NgOidcClientModule.forRoot({
+      oidc_config: {
+        authority: 'https://ng-oidc-client-server.azurewebsites.net',
+        client_id: 'ng-oidc-client-identity',
+        redirect_uri: 'http://localhost:4200/callback.html',
+        response_type: 'id_token token',
+        scope: 'openid profile offline_access api1',
+        post_logout_redirect_uri: 'http://localhost:4200/signout-callback.html',
+        silent_redirect_uri: 'http://localhost:4200/renew-callback.html',
+        automaticSilentRenew: true
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
