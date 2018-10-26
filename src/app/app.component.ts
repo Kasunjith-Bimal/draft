9db0b8e1a1299e3c287c8fe7044c8578a92f3d93
Alex <alexongithub@gmail.com>
2018-10-26 10:05:07 -0400

    add signin/signout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OidcFacade } from 'ng-oidc-client';
+import { User } from 'oidc-client';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'DraftApp';
+  identity$: Observable<User>;
+
+  constructor(private oidcFacade: OidcFacade) {
+    this.identity$ = this.oidcFacade.identity$;
+  }
+
+  ngOnInit() {
+    this.oidcFacade.getOidcUser();
+  }
+
+  signinRedirect() {
+    this.oidcFacade.signinRedirect();
+  }
+
+  signoutRedirect() {
+    this.oidcFacade.signoutRedirect();
+  }
 }
